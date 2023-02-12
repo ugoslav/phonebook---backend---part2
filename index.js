ff93f4b40ffceeaa6c2fed7bb75774adbabf73ae
Subharup Chakraborty <subharup2018@gmail.com>
2023-02-12 21:39:21 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
+app.use(express.static("build"))
 app.use(morgan("dev"))
 
 const exampleWare = (req,res,next) => {
